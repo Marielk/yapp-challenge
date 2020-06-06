8fb83a0519e700f684e2de473ede4d14b39d886d
Mariel-local <marielk.mya@gmail.com>
2020-06-06 19:14:40 -0400

    funcionalidades y estilos

--- src/components/PacientProfile.vue
+++ src/components/PacientProfile.vue
@@ -1,42 +1,59 @@
 
 <template>
-  <div class="container-table100">
-    <p>Pacientes works!</p>
-    <div>User {{ $route.query.patientID }}</div>
-    <li class="nav-item">
-      <router-link to="/" class="nav-link">Volver</router-link>
-    </li>
-      <div class="wrap-table100">
-        <div class="table100 ver1 m-b-110">
-          <div class="table100-head">
-            <table>
-              <thead>
-                <tr class="row100 head">
-                  <th class="cell100 column1">Class name</th>
-                  <th class="cell100 column2">Type</th>
-                  <th class="cell100 column3">Hours</th>
-                  <th class="cell100 column4">Trainer</th>
-                  <th class="cell100 column5">Spots</th>
-                </tr>
-              </thead>
-            </table>
+  <div class="container page-wrap">
+    <div class="d-flex justify-content-start">
+      <router-link to="/" class="nav-link back-btn">
+        <img class="arrow_back" src="../assets/back.png">
+          Volver
+      </router-link>
+    </div> 
+    
+    <h3 class="title-profile">Ficha del Paciente</h3>
+    <div class="card patient-card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-6">
+            <img class="user-profile" src="../assets/user1.png">
           </div>
- 
-          <div class="table100-body js-pscroll">
-            <table>
-              <tbody>
-                <tr class="row100 body">
-                  <td class="cell100 column1">Like a butterfly</td>
-                  <td class="cell100 column2">Boxing</td>
-                  <td class="cell100 column3">9:00 AM - 11:00 AM</td>
-                  <td class="cell100 column4">Aaron Chapman</td>
-                  <td class="cell100 column5">10</td>
-                </tr>
-              </tbody>
-            </table>
+          <div class="col-6 text-left">
+            <h6 class="font-weight-bold">Nombre del Paciente</h6>
+              <p v-if="patientsRecords[0]">{{patientsRecords[0].name}}</p>
+            <h6 class="font-weight-bold">Fecha de nacimiento</h6>
+              <p v-if="patientsRecords[0]">{{patientsRecords[0].dob}}</p>
+            <h6 class="font-weight-bold">Estatura</h6>
+              <p v-if="patientsRecords[0]">{{patientsRecords[0].height}}</p>
           </div>
         </div>
       </div>
+    </div>
+      <h3 class="subtitle-profile">Registro de ingresos del Paciente</h3>
+      <div class="table-container">
+        <div class="table-head-container">
+          <table class="row" style="height:50px">
+            <thead class="col-10 m-auto">
+              <tr class="d-flex justify-content-between">
+                <th>Fecha de ingreso</th>
+                <th>Diagnostico</th>
+                <th>Medico tratante</th>
+                <th>Peso</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+         <div class="table-body-container">
+            <table class="row">
+              <tbody class="col-10 m-auto">
+              <tr v-for="record in patientsRecords" :key="record.id" class="table-row spaced">
+                <td class="item-info celb-1">{{ record.date | moment("dddd, MMMM Do YYYY") }}</td>
+                <td class="item-info celb-2">{{record.diagnosis}} ({{record.diagnosisId}})</td>
+                <td class="item-info celb-3">{{record.doctor}}</td>
+                <td class="item-info cel-4">{{record.weight}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      
   </div>
 
 </template>
@@ -44,13 +61,97 @@
 <script>
 export default {
   name: 'PacientProfile',
-  props: {
-    
+  props: {},
+  data () {
+    return {
+      api: 'https://jsonmock.hackerrank.com/api/medical_records',
+      patientsRecords : []
+    }
+  },
+  created: function ()  {
+    this.fetchPatient()
+  },
+  methods: {
+    fetchPatient: function () {
+      this.$http.get(this.api+ `?userId${this.$route.query.patientID}`).then((response) => {
+          const allRecords = response.data.data
+          const formatedRecords = []
+          allRecords.map((record) => {
+            const formatedRecord = {
+              id: record.id,
+              name: record.userName,
+              dob: record.userDob,
+              height: record.meta.height,
+              weight: record.meta.weight,
+              date: this.formatDate(record.timestamp),
+              doctor: record.doctor.name,
+              diagnosis: record.diagnosis.name,
+              diagnosisId: record.diagnosis.id,
+              extras: record.vitals
+            }
+            // console.log(formatedRecord.date)
+            formatedRecords.push(formatedRecord)
+          })
+          this.patientsRecords = this.sortByDate(formatedRecords)
+      })
+    },
+    formatDate: function (date) {
+      const formatDate = this.$moment(date).format();
+      return formatDate;
+    },
+    sortByDate: function (arr) {
+      arr.sort((a, b) => {
+        const dateA = new Date(a.date)
+        const dateB = new Date(b.date)
+        return dateB - dateA
+      });
+      return arr
+    }
   }
 }
 </script>
 
 
 <style scoped>
+  .back-btn{
+    display: inline-flex;
+    align-items: center;
+    font-size: medium;
+  }
+  .arrow_back{
+    width: 5%;
+    margin-right: 5px;
+  }
+  .title-profile{
+    font-weight: 600;
+    margin-bottom: 1em;
+  }
+  .subtitle-profile{
+    font-weight: 500;
+    margin-bottom: 1em;
+    margin-top: 3em;
+  }
+  .patient-card{
+    width: 60%;
+    margin: auto;
+    box-shadow: 0 6px 6px -6px black;
+    padding-top: 15px;
+    border-radius: 6px;
+  }
+  .user-profile{
+    width: 50%;
+  }
 
+  .celb-1 {
+    width: 340px;
+  }
+  .celb-2 {
+    width: 290px;
+  }
+  .celb-3 {
+    width: 330px;
+  }
+  .spaced{
+    height: 50px;
+  }
 </style>
--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -1,45 +1,48 @@
 <template>
-  <div>
-    <div class="container">
-      <h1>Yapp Medical Challenge!</h1>
+    <div class="container page-wrap">
+      <h1 id="title">Yapp Medical Challenge!</h1>
+      <img id="main-img" src="../assets/diagnostic.svg">
       <div class="row">
-        <input>
+        <div class="col-12 mt-5 mb-5">
+          <h5>Buscar pacientes por nombre o apellido:</h5>
+          <input id="nameInput" v-on:keyup.enter ="filterByUser()" v-on:keyup.delete = "fetchFirst()" placeholder="Bob...">
+          <button v-on:click="filterByUser()" class="btn btn-info">Buscar</button>
+        </div>
       </div>
-    </div>
-    <div class="wrap-table100">
-        <div class="table100 ver1 m-b-110">
-          <div class="table100-head">
-            <table>
-              <thead>
-                <tr class="row100 head">
-                  <th class="cell100 column1">Nombre del Paciente</th>
-                  <th class="cell100 column2">Diagnostico</th>
-                  <th class="cell100 column3">Medico tratante</th>
-                  <th class="cell100 column4">Ver ficha</th>
+    
+      <div class="table-container">
+          <div class="table-head-container">
+            <table class="row" style="height:50px">
+              <thead class="col-10 m-auto">
+                <tr class="d-flex justify-content-between">
+                  <th class="">Nombre del Paciente</th>
+                  <th class="">Diagnostico</th>
+                  <th class="">Medico tratante</th>
+                  <th class="">...</th>
                 </tr>
               </thead>
             </table>
           </div>
  
-          <div class="table100-body js-pscroll">
-            <table>
-              <tbody>
-                <tr v-for="patient in patientsData" :key="patient.id" class="row100 body">
-                  <td class="cell100 column1">{{patient.userName}}</td>
-                  <td class="cell100 column2">{{patient.diagnosis.name}}</td>
-                  <td class="cell100 column3">{{patient.doctor.name}}</td>
-                  <td class="cell100 column4">
-                    <router-link :to="{ path: '/patient', query: { patientID: patient.userId }}" class="nav-link">ver pacientes</router-link>
+          <div class="table-body-container">
+            <table class="row">
+              <tbody class="col-10 m-auto">
+                <tr v-for="patient in patientsData" :key="patient.id" class="table-row">
+                  <td class="item-info cel-1">{{patient.name}}</td>
+                  <td class="item-info cel-2">{{patient.diagnosis}}</td>
+                  <td class="item-info cel-3">{{patient.doctor}}</td>
+                  <td class="item-info cel-4">
+                    <router-link :to="{ path: '/patient', query: { patientID: patient.id }}" class="nav-link">
+                      <span class="btn-ficha">Ir a la </span> ficha
+                      <img class="arrow btn-ficha" src="../assets/arrow.png">
+                    </router-link>
                   </td>
                 </tr>
               </tbody>
             </table>
           </div>
-        </div>
       </div>
-    <li class="nav-item">
-    </li>
-  </div>
+    </div>
 </template>
 
 <script>
@@ -48,27 +51,120 @@ export default {
   props: {},
   data () {
     return {
-      id: 1,
       api: 'https://jsonmock.hackerrank.com/api/medical_records',
-      patientsData: {}
+      patientsData: []
     }
   },
   created: function ()  {
-    this.test()
+    this.fetchFirst()
   },
   methods: {
-    test: function () {
+    fetchFirst: function () {
       this.$http.get(this.api).then((response) => {
-        this.patientsData = response.data.data
-        console.log(response)
+        const allData = response.data.data
+        this.patientsData = this.cleanData(allData)
       })
-    }
+    },
+    cleanData: function (data) {
+      // elimina los pacientes repetidos del listado
+      const reducedItems = [];
+      data.map((patient, index) => {
+        const patientResume = {
+          name: patient.userName ,
+          id: patient.userId,
+          doctor: patient.doctor.name,
+          diagnosis: patient.diagnosis.name
+        }
+        if (data[index+1]){
+          if (patient.userId !== data[index+1].userId) {
+            reducedItems.push(patientResume)
+          } 
+        } else {
+          reducedItems.push(patientResume)
+        }
+      })
+      return reducedItems   
+    },
+    filterByUser: function () {
+     const input = document.getElementById('nameInput').value
+      if (input.length >= 3) {
+        this.patientsData = this.patientsData.filter((patient) => {
+          const nameUpperCase = input.toUpperCase()
+          if (patient.name.toUpperCase().includes(nameUpperCase) === true) {
+            return patient
+          }
+        });
+      } 
+    },
   }
 }
 
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style>
+  .page-wrap{
+    background-image: url('../assets/background.jpg');
+    background-size: 100%;
+    background-repeat: no-repeat;
+    padding: 0;
+  }
+  #title{
+    background-color: cornflowerblue;
+    height: 90px;
+    border-radius: 5px;
+    color: white;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+  }
+  #main-img{
+    width: 10%;
+  }
+  #nameInput{
+    width: 290px;
+    border: 1px solid cornflowerblue;
+    height: 40px;
+  }
+  .table-container{
+    background-color: aliceblue;
+    border-radius: 5px;
+    padding: 0;
+    padding-bottom: 1em;
+  }
+  .table-head-container{
+    background-color: cornflowerblue;
+    color: white;
+    border-radius: 5px;
+    height: 50px;
+    margin-bottom: 1em;
+  }
+  .table-row{
+    border-bottom: 1px solid skyblue;
+  }
+  .item-info{
+    text-align: start;
+  }
+  .cel-1{
+    width: 340px;
+  }
+  .cel-2{
+    width: 270px;
+  }
+  .cel-3{
+    width: 198px;
+  }
+  .cel-4{
+    word-break: initial;
+  }
+  .arrow{
+    width: 20%;
+  }
+  .btn-ficha{
+    display: none;
+  }
+  @media (min-width: 992px) { 
+    .btn-ficha{
+      display: inline-flex;
+    }
+  }
 </style>
